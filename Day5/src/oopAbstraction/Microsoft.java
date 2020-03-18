a044986249af88e715b26f960fd5aa1d49fd8368
Evan Arafat <evanarafatbd@aol.com>
2020-03-17 23:31:30 -0400

    myfirst commit

---
+++ Day5/src/oopAbstraction/Microsoft.java
@@ -0,0 +1,21 @@
+package oopAbstraction;
+
+public class Microsoft extends Phone implements Computer{
+
+	
+	public void weight() {
+		System.out.println("Weight is 1lb");
+		
+	}
+	
+	public void games() {
+		System.out.println("I love to Paly games on my Phone");
+		
+	}
+
+		
+		
+	}
+	
+
+
